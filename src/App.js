48654dc5490a4f544daaa7ae0c28c281417a4e60
catonlan <catonlan@163.com>
2019-09-15 23:13:08 +0800

    导入semantic-ui 全局样式

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import  './app.less'
 import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom'
+// 导入semantic-ui 全局样式
+import 'semantic-ui-css/semantic.min.css'
+// 导入子组件
 import Login from './components/login/Login'
 import Layout from './components/layout/Layout'
 import NotFount from './components/404/404'
