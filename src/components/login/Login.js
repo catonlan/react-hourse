ef790cc265967e45478f656672a5dee5edbe3b45
catonlan <catonlan@163.com>
2019-09-16 21:14:59 +0800

    完成login页面

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,14 +1,54 @@
 import  React  from 'react'
 
-
+import  './Login.css'
+import {Form, FormButton} from 'semantic-ui-react'
 
 class Login extends React.Component {
+    constructor(props) {
+        super(props);
 
+        this.state = {
+            uname: '',
+            pwd: ''
+        }
+    }
+    login = () =>{
+        console.log('1111',this.state);
+    }
+
+    change = e => {
+        this.setState({
+            [e.target.name] : e.target.value
+        })
+    }
 
     render() {
+        
+            const {uname, pwd} = this.state;
         return (
-            <div>
-                    登录组件
+
+            <div className="login-container">
+                <div className="login-title">登录</div>
+                <div className="login-form">
+                    <Form onSubmit={this.login}>
+                        <Form.Field>
+                            <Form.Input icon="user" iconPosition="left"  placeholder="请输入用户名"
+                            name='uname'
+                             value={uname}
+                             onChange={this.change}
+                              />
+                        </Form.Field>
+                        <Form.Field>
+                            <Form.Input icon="lock" iconPosition="left"   placeholder="请输入密码" 
+                            name='pwd'
+                            type='password'
+                            value={pwd} 
+                            onChange={this.change}    
+                            />
+                        </Form.Field>
+                        <FormButton>登录</FormButton>
+                    </Form>
+                </div>
             </div>
         )
 
