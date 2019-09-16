a4b18baf06d3eb9d5f908ec57c0bbb0a26debe4c
catonlan <catonlan@163.com>
2019-09-16 21:52:41 +0800

    使用 axios 完成登录功能

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+// 导入 axios 让它成为 webpack 的依赖
+import './axios/index'
+// import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
